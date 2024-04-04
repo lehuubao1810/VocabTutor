57eba5493159240985e54653c34e033bc364b705
lehuubao1810 <lehuubao2909@gmail.com>
2024-04-04 19:18:28 +0700

    Add new pages and components

--- FE/Client/src/App.tsx
+++ FE/Client/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
+import { Home } from "./pages/Home";
+import { CharactersAI } from "./pages/CharactersAI";
+import { AddCollection } from "./pages/AddCollection";
+import { Collection } from "./pages/Collection";
+import { PageNotFound } from "./pages/PageNotFound";
+import { EditCollection } from "./pages/EditCollection";
+import { Learn } from "./pages/Learn";
+import { RoomAI } from "./pages/RoomAI";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isAuth = true;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={isAuth ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={isAuth ? <Navigate to="/" /> : <SignUp />}
+        />
+        {
+          !isAuth ? (
+            <Navigate to="/login" />
+          ) : (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/characters-ai" element={<CharactersAI />} />
+              <Route path="/add-collection" element={<AddCollection />} />
+              <Route path="/collection/:idCollection" element={<Collection />} />
+              <Route
+                path="/collection/:idCollection/edit"
+                element={<EditCollection />}
+              />
+              <Route path="/collection/:idCollection/learn" element={<Learn />} />
+              <Route path="/roomai/:idCharacterAI" element={<RoomAI />} />
+            </>
+          )
+        }
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- FE/Client/tailwind.config.js
+++ FE/Client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
