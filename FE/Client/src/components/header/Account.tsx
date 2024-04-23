a258886af73559dc1e46a33adae1f45e9343f287
Minh Hiển phan <phanminhhien0701@gmail.com>
2024-04-23 17:41:37 +0700

    Create Dropdown of User and fake data user

--- FE/Client/src/components/header/Account.tsx
+++ FE/Client/src/components/header/Account.tsx
@@ -1,17 +1,27 @@
-import React from "react";
-import Avatar from "../../assets/ImagesExample/Avatar.jpg";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-
+import UserDropDown from "./UserDropDown";
+import { dummyData } from "./data";
 interface AccountProps {
-	//id: number;
+	id: number;
+	username: string;
+	email: string;
+	avatar: string;
+	name: string;
+	role: string;
+	isOnline: boolean;
+	lastOnline: string;
 	notificationCount: number;
-	//avatar: string;
-	//name: string;
 }
-// const Account: React.FC<AccountProps> = ({ id, notificationCount, avatar, name }) => { ...}
-const Account: React.FC<AccountProps> = ({ notificationCount }) => {
+const Account: React.FC<AccountProps> = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const checkDropDown = () => {
+		setIsOpen((prevState) => !prevState);
+		console.log("Check drop down");
+	};
+	const userData = dummyData[0];
 	return (
 		<div className="flex items-center space-x-4">
 			{/* Button with '+' icon */}
@@ -22,18 +32,28 @@ const Account: React.FC<AccountProps> = ({ notificationCount }) => {
 			{/* Notification button */}
 			<button className="relative w-10 h-10 border-2 flex justify-center items-center rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 animate-shake max-md:hidden">
 				<span className="absolute top-0 right-1 -mt-1 -mr-1 bg-red-500 rounded-full w-4 h-4 flex items-center justify-center text-white text-xs">
-					{notificationCount}
+					{userData.notificationCount}
 				</span>
 				<FontAwesomeIcon icon={faBell} />
 			</button>
 
 			{/* Avatar */}
-			<button className="w-12 h-12 rounded-full overflow-hidden max-md:w-7 max-md:h-7">
-				<img className="w-full h-full object-cover" src={Avatar} alt="Avatar" />
-				<span className="absolute top-2 right-4 -mt-1 -mr-1 bg-red-500 rounded-full w-4 h-4 items-center justify-center text-white text-xs hidden max-md:flex ">
-					{notificationCount}
-				</span>
-			</button>
+			<div className="relative flex justify-center items-center w-12 h-12 max-md:w-10 max-md:h-10">
+				<button
+					onClick={checkDropDown}
+					className="w-12 h-12 rounded-full overflow-hidden max-md:w-7 max-md:h-7"
+				>
+					<img
+						className="w-full h-full object-cover"
+						src={userData.avatar}
+						alt="Avatar"
+					/>
+					<span className="absolute top-1 right-0 -mt-1 -mr-1 bg-red-500 rounded-full w-4 h-4 items-center justify-center text-white text-xs hidden max-md:flex ">
+						{userData.notificationCount}
+					</span>
+				</button>
+				<UserDropDown userData={userData} isOpen={isOpen} />
+			</div>
 		</div>
 	);
 };
---
+++ FE/Client/src/components/header/data.ts
@@ -0,0 +1,26 @@
+import Avatar from "../../assets/ImagesExample/Avatar.jpg";
+export interface UserData {
+	id: number;
+	username: string;
+	email: string;
+	avatar: string;
+	name: string;
+	role: string;
+	isOnline: boolean;
+	lastOnline: string;
+	notificationCount: number;
+}
+
+export const dummyData: UserData[] = [
+	{
+		id: 1,
+		username: "vanan99",
+		email: "vanan99@gmail.com",
+		avatar: Avatar,
+		name: "Nguyen Van An",
+		role: "",
+		isOnline: false,
+		lastOnline: "",
+		notificationCount: 2,
+	},
+];
